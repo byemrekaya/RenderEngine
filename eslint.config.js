4c4f16f459a5952a1b6318ab783947a5e8c2f86a
Emre Kaya <byemrkaya@gmail.com>
2025-04-22 20:11:44 +0200

    fix: husky and eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,60 +4,19 @@ const tseslint = require("@typescript-eslint/eslint-plugin");
 const tseslintParser = require("@typescript-eslint/parser");
 const angular = require("@angular-eslint/eslint-plugin");
 
-module.exports = [
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tseslintParser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-      "@angular-eslint": angular,
-    },
-    rules: {
-      ...eslint.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...tseslint.configs.stylistic.rules,
-      ...angular.configs.recommended.rules,
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-console": "off",
   },
-  {
-    files: ["**/*.spec.ts"],
-    languageOptions: {
-      globals: {
-        describe: "readonly",
-        it: "readonly",
-        expect: "readonly",
-        beforeEach: "readonly",
-      },
-    },
-  },
-  {
-    files: ["**/*.html"],
-    plugins: {
-      "@angular-eslint": angular,
-    },
-    rules: {
-      ...angular.configs.recommended.rules,
-    },
-  },
-];
+};
